47ee7c154cda5daaed84da3de60fb16e5ad1b348
Ork Hossain Muntaqin <60510097+orkhossain@users.noreply.github.com>
2022-03-22 17:10:51 +0100

    Add_imagePicker

--- DressApp/View Models/ClothViewModel.swift
+++ DressApp/View Models/ClothViewModel.swift
@@ -6,9 +6,9 @@
 //
 
 import Foundation
-import Firebase
-import CloudKit
-import FirebaseFirestoreSwift
+//import Firebase
+//import CloudKit
+//import FirebaseFirestoreSwift
 import SwiftUI
 
 
@@ -28,13 +28,61 @@ class ClothviewModel: ObservableObject {
     
     
     func addItem
-    (Description: String,Object: String, Item: String,Colour: String,Weather: String,Event: String,Gender: String,Favourite: Bool,Season: String)
+    (Description: String,Object: String, Item: String,Colour: String,Weather: String,Event: String,Gender: String,Favourite: Bool,Season: String,image: UIImage)
     {
-        db.collection("\(String(describing:Auth.auth().currentUser!.email))").addDocument(data: ["Description":Description,"Object": "Clothing", "Item":Item,"Colour": Colour, "Weather":Weather,"Event": Event, "Gender":Gender, "Favourite":Favourite,"Season": Season, "userID":userID])
+        
+        guard image.size.width == 0 else {
+            return
+        }
+        
+        let storageRef = Storage.storage().reference()
+
+        let imageData = image.jpegData(compressionQuality: 0.8)
+        
+        guard imageData != nil else {
+            return
+        }
+        let path = "\(Item)/\(UUID().uuidString).jpg"
+        let fileRef = storageRef.child(path)
+        let uploadTask = fileRef.putData(imageData!, metadata: nil){ metadata,
+            error in
+            
+            if error == nil && metadata != nil {
+//                 let user = "\(String(describing:Auth.auth().currentUser!.email))"
+                 let db = Firestore.firestore()
+                 let userID = Auth.auth().currentUser!.uid
+                
+                db.collection("\(String(describing:Auth.auth().currentUser!.email))").addDocument(data: ["Description":Description,"Object": "Clothing", "Item":Item,"Colour": Colour, "Weather":Weather,"Event": Event, "Gender":Gender, "Favourite":Favourite,"Season": Season, "userID":userID,"Image": path])
+            }
+            
+        }
+
         
     }
     
-    
+//    func uploadImage(image: UIImage, path: String){
+//        guard image.size.width == 0 else {
+//            return
+//        }
+//
+//        let storageRef = Storage.storage().reference()
+//
+//        let imageData = image.jpegData(compressionQuality: 0.8)
+//
+//        guard imageData != nil else {
+//            return
+//        }
+//
+//        let fileRef = storageRef.child("\(path)/\(UUID().uuidString).jpg")
+//        let uploadTask = fileRef.putData(imageData!, metadata: nil){ metadata,
+//            error in
+//
+//            if error == nil && metadata != nil {
+//
+//            }
+//
+//        }
+//    }
     
     func getClothing(){
         db.collection(user).whereField("Object", isEqualTo: "Clothing")
