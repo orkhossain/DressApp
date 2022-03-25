357b0bf0a0c77589b6e948003e8f048bb6df41af
Ork Hossain Muntaqin <60510097+orkhossain@users.noreply.github.com>
2022-03-25 12:29:29 +0100

    Update matching algorithm

--- DressApp/View Models/OutfitViewModel.swift
+++ DressApp/View Models/OutfitViewModel.swift
@@ -29,7 +29,6 @@ class OutfitViewModel: ObservableObject {
     
     
     func getOutfits(){
-        self.autoDelete()
         db.collection(user).whereField("Object", isEqualTo: "Outfit")
             .addSnapshotListener{ (querySnapshot, error) in
                 
@@ -56,19 +55,6 @@ class OutfitViewModel: ObservableObject {
             }
     }
     
-    
-    func autoDelete(){
-        
-        db.collection(user).whereField("Object", isEqualTo: "Outfit").whereField("Clothing", isEqualTo: []).getDocuments(){ (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                DispatchQueue.main.async {
-                    for document in querySnapshot!.documents {
-                        document.reference.delete()
-                    }
-                }}
-        }}
     
     
     
@@ -108,25 +94,14 @@ class OutfitViewModel: ObservableObject {
             { error in
                 DispatchQueue.main.async{
                     if error == nil {
-                        self.getFavourite()
-                    }
-                }
-            }
-        }
+                        self.getFavourite()}}}}
         else
-        {
-            db.collection(user).document(Outfit.id).setData(["Favourite": false],merge: true){
+        {db.collection(user).document(Outfit.id).setData(["Favourite": false],merge: true){
                 error in
                 DispatchQueue.main.async{
                     if error == nil {
                         self.getFavourite()
-                    }}
-            }
-            
-            
-        }
-        
-    }
+                    }}}}}
     
     
     func deleteData(OutfitToDelete: Outfit){
@@ -134,94 +109,159 @@ class OutfitViewModel: ObservableObject {
             DispatchQueue.main.async {
                 if error == nil {
                     self.list.removeAll() { Outfit in
-                        return OutfitToDelete.id == Outfit.id
-                    }
-                }
-            }
-            
-        }
-        
+                        return OutfitToDelete.id == Outfit.id}}}}}
+    
+    
+    
+func generateOutfit(Weather: String,minTemp: String, maxTemp: String, gender: String, Event: String, Clothings: [Clothing], Outifits: [Outfit]){
+    
+    var weather: String
+    switch Weather{
+    case "Clear":
+        weather = "Sunny"
+    case "Clouds":
+        weather = "Cloudy"
+    case "Rain":
+        weather = "Rainy"
+    case "Snow":
+        weather = "Snow"
+    case "Drizzle":
+        weather = "Rainy"
+    case "Thunderstorm":
+        weather = "Rainy"
+    default:
+        weather = ""
     }
     
     
+    let Season = getSeason()
     
-    func generateOutfit(Weather: String,minTemp: String, maxTemp: String, gender: String, Event: String, Clothings: [Clothing], Outifits: [Outfit]){
+    let weatherList = Clothings.filter{$0.Weather.contains(weather)}
+    var genderList = [Clothing]()
+    if gender == "Unisex"{
+        genderList =  weatherList.filter{$0.Gender.contains("Unisex")}
         
-        var weather: String
-        switch Weather{
-        case "Clear":
-            weather = "Sunny"
-        case "Clouds":
-            weather = "Cloudy"
-        case "Rain":
-            weather = "Rainy"
-        case "Snow":
-            weather = "Snow"
-        case "Drizzle":
-            weather = "Rainy"
-        case "Thunderstorm":
-            weather = "Rainy"
-        default:
-            weather = ""
+    }
+    else{
+        genderList = weatherList.filter{$0.Gender.contains(gender)} + weatherList.filter{$0.Gender.contains("Unisex")}
+    }
+    
+    
+    let list = genderList.filter{$0.Event.contains(Event)}
+    
+    
+
+    let combinationList = generateCombination(Clothings: Clothings, Outifits: Outifits)
+    
+    let a = combinationList[Event]! as [[Dictionary<String,String>]]
+    
+    var e = [[[[String:String]]]]()
+    for i in a{
+        if (i.count > 2){
+            let b =  (i.combinations(length: 2, repeatingElements: false))
+            e.append(b)
         }
+        if(i.count <= 2){
+            e.append([i])}
+    }
+    
+    let c = Array(e.joined()) as [[[String:String]]]
+
+    var j = [[[String:String]]]()
+    
+    for i in c {
+        let ii: [Dictionary<String,String>] = i
+        let jj =  (ii.sorted(by: {itemsSort(p1: $0, p2: $1)})) as [Dictionary<String,String>]
+        j.append(jj)
+    }
+
+    let d = j.reduce(into: [:]) { counts, number in
+       counts[number, default: 0] += 1
+
+    }
+    
+    let dd = d.sorted { (a: ([[String : String]], Int), b:([[String : String]], Int) ) in
+        a.1 < b.1
+    }
+    
+    var newDict =  [[[String:String]]: Int]()
+    for i in dd{
+        newDict[i.key] = i.value
+    }
+    print(newDict)
+
+    
+}
+    
+func generateCombination(Clothings: [Clothing], Outifits: [Outfit]) -> Dictionary<String,[[Dictionary<String,String>]]>{
+    
+    var EventOutfitsDictionary = Dictionary<String,[[Dictionary<String,String>]]>()
+
+    for outfit in Outifits{
+        let k = outfit.Event
+        let outFitDetail = outfit.Clothing
+
+        var outFitDetails = [[Dictionary<String,String>]]()
+        var clothingDetails = [Dictionary<String,String>]()
+
+        for (key,_) in outFitDetail {
+
+                let clothList = Clothings.filter({$0.id.contains("\(key)")})
+                for cloth in clothList {
+                    clothingDetails.append([cloth.Item:cloth.Colour])}
+            }
         
-        let weatherList = Clothings.filter{$0.Weather.contains(weather)}
-        var genderList = [Clothing]()
-        if gender == "Unisex"{
-            genderList =  weatherList.filter{$0.Gender.contains("Unisex")}
-            
+        if !outFitDetails.contains(clothingDetails){
+            outFitDetails.append(clothingDetails)}
+
+        if EventOutfitsDictionary[k] != nil{
+            EventOutfitsDictionary[k]?.append(contentsOf: outFitDetails)
         }
         else{
-            genderList = weatherList.filter{$0.Gender.contains(gender)} + weatherList.filter{$0.Gender.contains("Unisex")}
+            EventOutfitsDictionary[k] = outFitDetails
         }
-        
-        
-        let list = genderList.filter{$0.Event.contains(Event)}
-        
-        
-        var EventOutfitsDictionary = Dictionary<String,[[Dictionary<String,String>]]>()
-
-        for outfit in Outifits{
-            let k = outfit.Event
-            let outFitDetail = outfit.Clothing
-
-            var outFitDetails = [[Dictionary<String,String>]]()
-            var clothingDetails = [Dictionary<String,String>]()
-
-            for (key,_) in outFitDetail {
-
-                    let clothList = Clothings.filter({$0.id.contains("\(key)")})
-                    for cloth in clothList {
-                        clothingDetails.append([cloth.Item:cloth.Colour])}
-                }
-            
-            if !outFitDetails.contains(clothingDetails){
-                outFitDetails.append(clothingDetails)}
-
-            if EventOutfitsDictionary[k] != nil{
-                EventOutfitsDictionary[k]?.append(contentsOf: outFitDetails)
-            }
-            else{
-                EventOutfitsDictionary[k] = outFitDetails
-            }
-        }
-        
-        
-        let a = EventOutfitsDictionary[Event]! as [[Dictionary<String,String>]]
-        let b =  a.combinations(length: 2, repeatingElements: false)
-        let c = Array(b.joined())
-        let d = c.reduce(into: [:]) { counts, number in
-            counts[number, default: 0] += 1
-        }
-        let date = Date()
-        print(date)
-        
-        
-        
-        
-        
     }
     
+    return EventOutfitsDictionary
+    
+}
     
     
+    
+func itemsSort(p1:[String:Any], p2:[String:Any]) -> Bool {
+        guard let s1 = p1["itemName"] as? String, let s2 = p2["itemName"] as? String else {
+            return false
+        }
+        return s1 < s2
+    }
+
+
+
+
+
+func getSeason() -> String {
+    
+    let now = Date()
+    let day  = Calendar.iso8601.ordinality(of: .day, in: .year, for: now)!
+    var Season : String
+    
+    switch day {
+       case 80..<172:
+        Season = "Spring"
+       case 172...264:
+        Season = "Summer"
+        case 265...355:
+        Season = "Autumn"
+    default:
+        Season = "Winter"
+    }
+    
+    return String(Season)
+    
+}
+
+}
+
+extension Calendar {
+    static let iso8601 = Calendar(identifier: .iso8601)
 }
